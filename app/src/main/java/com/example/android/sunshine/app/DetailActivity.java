96e17662d317574ffdce155cc0cfc77a06a9cbba
Rogger Matamoros <roggmatz@gmail.com>
2016-06-08 18:50:20 -0400

    Display Intent Contents in TextView
    The contents of the intent passed by forecastFragment are displayed on
    testTextView

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,20 +1,28 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
+    static final String LOG_TAG = DetailActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailFragment()).commit();
+        }
     }
 
     @Override
@@ -35,13 +43,24 @@ public class DetailActivity extends ActionBarActivity {
         }
     }
 
-    public static class PlaceholderFragment extends Fragment {
-        public PlaceholderFragment() {}
+    public static class DetailFragment extends Fragment {
+        public DetailFragment() {}
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_detail, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            Intent intent = getActivity().getIntent();
+
+            //check that an Intent exists and contains EXTRA_TEXT before attempting
+            //to assign its value to testTextView
+            if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                ((TextView) rootView.findViewById(R.id.testTextView))
+                        .setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+            }
+
+            return rootView;
         }
     }
 }
