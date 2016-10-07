162860344aff3b8b253d2baaab8994b60495f419
Rogger Matamoros <roggmatz@gmail.com>
2016-10-07 17:43:55 -0400

    Implemented Share menu.

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -2,18 +2,21 @@ package com.example.android.sunshine.app;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
-    static final String LOG_TAG = DetailActivity.class.getSimpleName();
+    static final String LOG_TAG = DetailFragment.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         //add items to action bar if present
         getMenuInflater().inflate(R.menu.detail, menu);
+
         return true;
     }
 
@@ -46,7 +50,15 @@ public class DetailActivity extends ActionBarActivity {
     }
 
     public static class DetailFragment extends Fragment {
-        public DetailFragment() {}
+
+        public DetailFragment() {
+            //Tells fragment to call onCreateOptionsMenu()
+            setHasOptionsMenu(true);
+        }
+
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String SHARE_HASHTAG = "#sunshineapp";
+        private String forecastString;
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,9 +72,41 @@ public class DetailActivity extends ActionBarActivity {
             if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
                 ((TextView) rootView.findViewById(R.id.testTextView))
                         .setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+                forecastString = intent.getStringExtra(Intent.EXTRA_TEXT);
             }
 
             return rootView;
+        }
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+            //get share item
+            MenuItem shareItem = menu.findItem(R.id.action_share);
+
+            //get share provider
+            ShareActionProvider shareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+            //add an intent to the share provider.
+            if(shareActionProvider != null) {
+                shareActionProvider.setShareIntent(makeShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Provider is null.");
+            }
+        }
+
+        private Intent makeShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+            //keeps from shared to application to be added into Sunshine's
+            //activity stack. IE: tapping back returns user to Sunshine and not
+            //the shared app main activity.
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(
+                    Intent.EXTRA_TEXT,
+                    forecastString + " " + SHARE_HASHTAG);
+            return shareIntent;
         }
     }
 }
